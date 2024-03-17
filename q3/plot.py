7462875c212bd748469d7aec4b658075cb7fd466
willow-hu <willow_hu@outlook.com>
2024-03-17 11:10:53 +0800

    q3

--- q3/plot.py
+++ q3/plot.py
@@ -5,6 +5,8 @@ def plot_trajectory(x, y, title: str):
     plt.title(title)
     plt.xlabel('X coordinate (pixels)')
     plt.ylabel('Y coordinate (pixels)')
+    plt.xlim(right=1920) 
+    plt.ylim(top=1080)
     # plt.gca().set_aspect('equal')  # Set the same scale for both axes
     plt.gca().invert_yaxis()  # The origin is at the top-left corner
     plt.grid(True)
